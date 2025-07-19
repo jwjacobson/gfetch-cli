e131f7b1d356e7e7f2626ff5a41fb8fcdc95a061
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-07-19 10:29:51 -0400

    Add second create_dirs test, add assertions to both

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from app import create_dirs
 
 
-def test_create_dirs(fake_dir_config_paths_only):
+def test_create_dirs_dont_exist(fake_dir_config_paths_only):
     raw_path = fake_dir_config_paths_only.RAW_EMAIL_DIR
     clean_path = fake_dir_config_paths_only.CLEAN_EMAIL_DIR
     attachments_path=fake_dir_config_paths_only.ATTACHMENTS_DIR
@@ -15,3 +15,34 @@ def test_create_dirs(fake_dir_config_paths_only):
     assert raw_path.exists()
     assert clean_path.exists()
     assert attachments_path.exists()
+
+    assert raw_path.is_dir()
+    assert clean_path.is_dir()
+    assert attachments_path.is_dir()
+
+    assert not any(raw_path.iterdir())
+    assert not any(clean_path.iterdir())
+    assert not any(attachments_path.iterdir())
+
+def test_create_dirs_already_exist(fake_dir_config):
+    raw_path = fake_dir_config.RAW_EMAIL_DIR
+    clean_path = fake_dir_config.CLEAN_EMAIL_DIR
+    attachments_path=fake_dir_config.ATTACHMENTS_DIR
+
+    assert raw_path.exists()
+    assert clean_path.exists()
+    assert attachments_path.exists()
+    
+    create_dirs(fake_dir_config)
+
+    assert raw_path.exists()
+    assert clean_path.exists()
+    assert attachments_path.exists()
+
+    assert raw_path.is_dir()
+    assert clean_path.is_dir()
+    assert attachments_path.is_dir()
+
+    assert not any(raw_path.iterdir())
+    assert not any(clean_path.iterdir())
+    assert not any(attachments_path.iterdir())
